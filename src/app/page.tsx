b2d5d31fac2c5620ee8418cab31269699fab7d90
Ben Ku <benku12803@gmail.com>
2023-10-29 14:28:59 -1000

    added about card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,44 @@
 import Image from 'next/image';
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '@/components/ui/card';
 import Nav from '@/components/ui/nav';
-import { Calendar } from '@/components/ui/calendar';
-import { Button } from '@/components/ui/button';
-import { ModeToggle } from '@/components/ui/theme-toggle';
+import Link from 'next/link';
+import { Link as LinkIcon } from 'lucide-react';
+import { TypographyH2, TypographyLarge } from '@/components/typography/headings';
+import { TypographyP } from '@/components/typography/text';
 
 export default function Home() {
   return (
-    <main className="md:container">
+    <main className="sm:container">
       <Nav />
+      <section id="home" className="mt-[3rem] p-1 h-[calc(100vh-3rem)]">
+        <div className="flex h-full items-center">
+          <Card>
+            <CardHeader>
+              <CardTitle>About</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <TypographyH2>Hi, I'm <span className="text-primary">Ben</span></TypographyH2>
+                <TypographyLarge><span className="text-destructive">Aspiring Software Developer</span></TypographyLarge>
+                <CardDescription className="py-6 max-w-md">
+                  I am currently studying Mechanical Engineering, but have a passion for providing solutions through code to build applications.
+                </CardDescription>
+            </CardContent>
+            <CardFooter className="pb-1">
+              <LinkIcon />&nbsp;Links
+            </CardFooter>
+            <CardContent>
+              <i className="flex">
+                <Link href="https://www.linkedin.com/in/benybku" target="_blank" rel="noopener noreferrer">
+                  <Image src="icons/linkedin.svg" alt="linkedin" width={48} height={48} />
+                </Link>
+                <Link href="https://github.com/kub12803" target="_blank" rel="noopener noreferrer">
+                  <Image src="icons/github.svg" alt="github" width={48} height={48} />
+                </Link>
+              </i>
+            </CardContent>
+          </Card>
+        </div>
+      </section>
     </main>
   )
 }
--- src/components/ui/nav.tsx
+++ src/components/ui/nav.tsx
@@ -4,14 +4,14 @@ import { ModeToggle } from "./theme-toggle";
 
 export default function Nav() {
     return (
-        <nav className="sticky top-0 border-b">
+        <nav className="fixed w-full left-0 top-0 border-b bg-inherit z-1000">
             <div className="sm:container flex p-1 items-center">
                 <div className="flex-grow">
                     <TypographyH4>{'<'} <span className="text-primary">Ben</span> {'/>'}</TypographyH4>
                 </div>
-                <ul className="flex items-center space-x-4">
+                <ul className="flex items-center space-x-6">
                     <li>Home</li>
-                    <li>Tools & Skills</li>
+                    <li>Projects</li>
                     <li>Resume</li>
                     <li><ModeToggle /></li>
                 </ul>
