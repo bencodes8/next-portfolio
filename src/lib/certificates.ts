a7b3db4e9b3974807814396e3c59d6130d732027
Ben Ku <110452216+kub12803@users.noreply.github.com>
2023-12-12 16:10:52 -1000

    added certificate

--- src/lib/certificates.ts
+++ src/lib/certificates.ts
@@ -14,5 +14,12 @@ export const certificates: Certificate[] = [
       href: 'https://certificates.cs50.io/a23d55f5-13bf-4e6d-a055-d48f327bb369.pdf?size=letter',
       description: 'Course pickups where CS50x lefts off, diving more deeply into the design and implementation of web applications.',
       year_received: 2023
+    },
+    {
+      displayName: 'CS50SQL',
+      imageSrc: 'harvard-logo',
+      href: 'https://certificates.cs50.io/efa9ed13-5423-4e5c-8a99-e2b64e013603.pdf?size=letter',
+      description: 'Courses provided foundation using SQL to create, read, update, and delete relational databases. Also learned how to model real-world entities, normalize data, and the trade-offs when indexing tables within databases.',
+      year_received: 2023
     }
 ];
